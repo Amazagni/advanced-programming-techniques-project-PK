bec0048da7e8db225eda580f195525dbfd14503f
Amazagni <marcinxkomputer@gmail.com>
2025-03-09 19:58:31 +0100

    Frontend: navbar, quantity change, add item component added

---
+++ frontend/web-app/src/app/views/add-item-view/add-item-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-item-view.component.html',
+  styleUrl: './add-item-view.component.css'
+})
+export class AddItemViewComponent {
+
+}
