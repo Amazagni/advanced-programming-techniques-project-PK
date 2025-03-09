53cd4c0e802d8d2f9d73ab19361b76bc76b36cd9
Amazagni <marcinxkomputer@gmail.com>
2025-03-09 08:27:11 +0100

    Frontend: main views created

---
+++ frontend/web-app/src/app/views/home-view/home-view.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-home-view',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './home-view.component.html',
+  styleUrl: './home-view.component.css'
+})
+export class HomeViewComponent {
+
+  ngOnInit() {
+    console.log("MAIN VIEW")
+  }
+}
--- frontend/web-app/src/main.ts
+++ frontend/web-app/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter} from '@angular/router';
+import {HomeViewComponent} from './app/views/home-view/home-view.component';
+import {InventoryViewComponent} from './app/views/inventory-view/inventory-view.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: HomeViewComponent },
+      { path: 'inventory', component: InventoryViewComponent }
+    ])
+  ]
+}).catch(err => console.error(err));
