dcb07cb2777e0af3a94a832f7760d4b8910f22ee
Amazagni <marcinxkomputer@gmail.com>
2025-03-10 19:07:58 +0100

    Frontend: add item view

--- frontend/web-app/src/app/views/add-item-view/add-item-view.component.ts
+++ frontend/web-app/src/app/views/add-item-view/add-item-view.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Item} from '../../models/item.model';
 
 @Component({
   selector: 'app-add-item-view',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-item-view.component.html',
   styleUrl: './add-item-view.component.css'
 })
 export class AddItemViewComponent {
+  itemForm: FormGroup;
+  selectedImage: File | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.itemForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      quantity: [1, [Validators.required, Validators.min(1)]],
+      imageUrl: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if (this.itemForm.valid && this.selectedImage) {
+      const newItem: Item = {
+        ...this.itemForm.value,
+        image: this.selectedImage.name
+      };
+      console.log('Nowy item:', newItem);
+    }
+  }
+
+  onImageSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedImage = fileInput.files[0];
+      console.log('Wybrany obrazek:', this.selectedImage);
+    }
+  }
 }
